18dbd2c32e81061110b8c7c13f1e908300339314
AUMY20 <99035834+AUMY20@users.noreply.github.com>
2022-08-05 20:54:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,10 +7,31 @@ function setup()
 {
     canvas = createCanvas(300,300);
     canvas.center();
-    
+    cam = createCapture(VIDEO);
+    cam.size(300,300) ;cam.hide();
+    union = ml5.poseNet(cam,modelLoded);
+    union.on('pose',gotPoses);
+}
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        console.log(results[0].pose.nose.x);
+        console.log(results[0].pose.nose.y);
+    }
+}
+
+function modelLoded()
+{
+console.log("check");
 }
 
 function draw()
+{
+    image(cam,0,0,300,300);
+
+}
 
 function snap_save()
 {
